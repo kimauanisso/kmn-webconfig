2b141e853b76d145dd6fdfe03ed2ab27f6ad9fd6
Lucas Calçada <lucasmgmcalcada@gmail.com>
2026-02-23 00:13:45 -0300

    feat: update config.h

--- include/kmn_webconfig/config.h
+++ include/kmn_webconfig/config.h
@@ -1,8 +1,13 @@
 #pragma once
 
 #ifndef MAX_ROUTES
-#define MAX_ROUTES 2
+#define MAX_ROUTES 16
 #endif
 
 #define MAX_RES_SIZE 4000
 #define MAX_REQ_SIZE 4000
+
+#ifndef VARIABLE_COUNT
+#warning VARIABLE_COUNT not defined, defaulting to 16
+#define VARIABLE_COUNT 16
+#endif
