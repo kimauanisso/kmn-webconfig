46afa929559cccca2e7f1a2f4049fa1eaebf50df
LucasCalcada <lucasmgmcalcada@gmail.com>
2026-02-26 20:02:09 -0300

    fix: fix some routes

--- src/handlers/dashboardHandler.c
+++ src/handlers/dashboardHandler.c
@@ -4,7 +4,7 @@
 void dashHandler(HttpBuffer *req, HttpBuffer *res) {
   build_res_start(res, 200, "OK");
   build_res_header(res, "Content-Type", "text/html; charset=utf-8");
-  res_buffer_append(res, "Content-Length: %d\n", index_html_len);
+  res_buffer_append(res, "Content-Length: %d\r\n", index_html_len);
   build_res_header(res, "Connection", "close");
   res_buffer_append(res, "\r\n");
   res_buffer_append(res, index_html);
--- src/handlers/notFoundHandler.c
+++ src/handlers/notFoundHandler.c
@@ -4,4 +4,6 @@
 
 void notFoundHandler(HttpBuffer *req, HttpBuffer *res) {
   build_res_start(res, 404, "Not Found");
+  build_res_header(res, "Connection", "close");
+  res_buffer_append(res, "\r\n");
 }
